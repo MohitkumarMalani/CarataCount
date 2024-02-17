6316caaa1d46769365b0d5c9507c5d87858b0b62
mohitkumarmalani <mmalani9749@conestogac.on.ca>
2024-02-17 03:00:08 -0500

    add application user

---
+++ CaratCount/Data/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+﻿using CaratCount.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaratCount.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public static async Task CreateAdminUser(IServiceProvider serviceProvider)
+        {
+            UserManager<ApplicationUser> userManager =
+                serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            RoleManager<IdentityRole> roleManager = serviceProvider
+                .GetRequiredService<RoleManager<IdentityRole>>();
+
+            string email = "admin@caratcount.com";
+            string password = "Admin@159#";
+            string roleName = "Admin";
+            string phoneNumber = "1234567890";
+            string gstInNo = "22AAAAA0000A1Z5";
+
+            // if role doesn't exist, create it
+            if (await roleManager.FindByNameAsync(roleName) == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+
+            // if username doesn't exist, create it and add it to role
+            if (await userManager.FindByEmailAsync(email) == null)
+            {
+                ApplicationUser user = new ApplicationUser { Email = email, PhoneNumber = phoneNumber, GstInNo = gstInNo };
+                var result = await userManager.CreateAsync(user, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, roleName);
+                }
+            }
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+       : base(options)
+        {
+        }
+
+        
+    }
+}
---
+++ CaratCount/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace CaratCount.Models
+{
+
+    public class ApplicationUser : IdentityUser
+    {
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone(ErrorMessage = "Invalid phone number")]
+        public override string? PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "GSTIN is required")]
+        [RegularExpression(@"^\d{2}[A-Z]{5}\d{4}[A-Z]\d{1}$", ErrorMessage = "GSTIN must be in the format 00AAAAA0000A0Z0")]
+        public string? GstInNo { get; set; }
+    }
+}

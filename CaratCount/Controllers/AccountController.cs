a28eb9c4f3d6816dde1c0f33a50fc3b49f65a5d7
mohitkumarmalani <mmalani9749@conestogac.on.ca>
2024-02-17 03:05:24 -0500

    Update Login.cshtml

---
+++ CaratCount/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using CaratCount.Models;
+using CaratCount.Models.Account;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaratCount.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger<HomeController> _logger;
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ILogger<HomeController> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        // GET: /account/login
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewBag.PageName = "Login";
+            var model = new LoginViewModel { ReturnUrl = returnUrl };
+            return View(model);
+        }
+
+        // POST: /account/login
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> LogIn(LoginViewModel model)
+        {
+            _logger.LogInformation("login",model.RememberMe.ToString());
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.UserEmail, model.Password,
+                            isPersistent: model.RememberMe, lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+
+            ViewBag.PageName = "Login";
+            ModelState.AddModelError("", "Invalid username/password.");
+            return View(model);
+        }
+    }
+}
---
+++ CaratCount/Models/Account/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CaratCount.Models.Account
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Please enter a email.")]
+        [StringLength(255)]
+        public string? UserEmail { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        [StringLength(255)]
+        public string? Password { get; set; }
+
+        public string? ReturnUrl { get; set; }
+
+        public bool RememberMe { get; set; } = false;
+    }
+}
